15558e7dc5fbaed9781b1797d8fe6d7e5f0ce9af
gitascnuser <cnuser_april30@hotmail.com>
2026-03-24 18:10:05 +0800

    Update Test1P.java
    remote update file

--- Test1/src/Test1P/Test1P.java
+++ Test1/src/Test1P/Test1P.java
@@ -4,7 +4,7 @@ public class Test1P {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-       System.out.println("oooupdate1_update message of branch1");
+       System.out.println("oooupdate1_update message of branch1_remote update");
 	}
 
 }
