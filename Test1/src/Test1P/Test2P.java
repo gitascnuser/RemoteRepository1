6e6b8e500f10ffb7b386341633cb07ad6676a604
gitascnuser <cnuser_april30@hotmail.com>
2026-03-26 09:57:32 +0800

    I need to add Test2P file

---
+++ Test1/src/Test1P/Test2P.java
@@ -0,0 +1,17 @@
+package Test1P;
+
+
+public class Test2P {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+       System.out.println("oooupdate1");
+       System.out.println("oooupdate2");
+       System.out.println("oooupdate3");
+       System.out.println("oooupdate4");
+       System.out.println("oooupdate5");
+	}
+
+}
+
